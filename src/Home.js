29c1d60d8d617ad4595bb8b63f5808453f796ab4
kev <kbreto2911@gmail.com>
2021-10-07 00:03:47 -0400

    buttons functionality and animations

--- src/Home.js
+++ src/Home.js
@@ -3,12 +3,14 @@ import Header from './components/Header.js'
 import Favorites from './components/Favorites.js'
 import { githubLogo, gmailLogo, youtubeLogo, linkedinLogo } from './images.js'
 
-const AppContainer = ({image, alt, children}) => {
+const HomeAppButton = ({href, image, alt, children}) => {
 
 	return (
 		<li className={styles.li} >
-			<img className={styles.img} src={image} alt={alt} ></img>
-			<p className={styles.text} >{children}</p>
+			<a className={styles.a} href={href} >
+				<img className={styles.img} src={image} alt={alt} ></img>
+				<p className={styles.text} >{children}</p>
+			</a>
 		</li>
 	);
 }
@@ -17,10 +19,10 @@ const Home = () => {
 	return (
 		<div className={styles.container}>
 			<ul className={styles.ul} >
-				<AppContainer image={gmailLogo} alt="gmail logo" >gmail</AppContainer>
-				<AppContainer image={youtubeLogo} alt="youtube logo" >youtube</AppContainer>	
-				<AppContainer image={githubLogo} alt="github logo" >github</AppContainer>	
-				<AppContainer image={linkedinLogo} alt="linkedin logo" >linkedin</AppContainer>	
+				<HomeAppButton href="https://mail.google.com/mail/u/0/" image={gmailLogo} alt="gmail logo" >gmail</HomeAppButton>
+				<HomeAppButton href="https://www.youtube.com" image={youtubeLogo} alt="youtube logo" >youtube</HomeAppButton>	
+				<HomeAppButton href="https://www.linkedin.com/feed/" image={linkedinLogo} alt="linkedin logo" >linkedin</HomeAppButton>
+				<HomeAppButton href="https://github.com" image={githubLogo} alt="github logo" >github</HomeAppButton>	
 
 			</ul>
 		</div>
